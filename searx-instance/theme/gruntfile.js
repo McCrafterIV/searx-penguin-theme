4e99bd2c1a8a9b38e33bae05f137e779eeb16909
McCrafterIV <38863733+McCrafterIV@users.noreply.github.com>
2021-12-31 15:18:47 +0100

    General search layout responsive

--- searx-instance/theme/gruntfile.js
+++ searx-instance/theme/gruntfile.js
@@ -1,116 +1,107 @@
 /*jshint esversion: 6 */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    copy: {
-      js: {
-        expand: true,
-        cwd: './node_modules',
-        dest: './js/',
-        flatten: true,
-        filter: 'isFile',
-        timestamp: true,
-        src: [
-          './jquery/dist/jquery.min.js',
-          './leaflet/dist/leaflet.js',
-        ]
-      },
-      css: {
-        expand: true,
-        cwd: './node_modules',
-        dest: './css/',
-        flatten: true,
-        filter: 'isFile',
-        timestamp: true,
-        src: [
-          './leaflet/dist/leaflet.css',
-          './bulma/css/bulma.min.css',
-        ]
-      },
-      icons: {
-        expand: true,
-        cwd: './node_modules',
-        dest: './img/icons/',
-        flatten: true,
-        filter: 'isFile',
-        timestamp: true,
-        src: [
-          './feather-icons/dist/feather-sprite.svg',
-        ]
-      },
-      leaflet_images: {
-        expand: true,
-        cwd: './node_modules',
-        dest: './css/images/',
-        flatten: true,
-        filter: 'isFile',
-        timestamp: true,
-        src: [
-          './leaflet/dist/images/*.png',
-        ]
-      }
-    },
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['src/js/*.js'],
-        dest: 'js/searx.js'
-      }
-    },
-    uglify: {
-      options: {
-        sourceMap: true,
-        banner: '/*! oscar/searx.js | <%= grunt.template.today("dd-mm-yyyy") %> | <%= process.env.GIT_URL %>  */\n'
-      },
-      dist: {
-        files: {
-          'js/searx.min.js': ['<%= concat.dist.dest %>']
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            js: {
+                expand: true,
+                cwd: './node_modules',
+                dest: './js/',
+                flatten: true,
+                filter: 'isFile',
+                timestamp: true,
+                src: [
+                    './jquery/dist/jquery.min.js',
+                    './leaflet/dist/leaflet.js',
+                ]
+            },
+            css: {
+                expand: true,
+                cwd: './node_modules',
+                dest: './css/',
+                flatten: true,
+                filter: 'isFile',
+                timestamp: true,
+                src: [
+                    './leaflet/dist/leaflet.css',
+                    './bulma/css/bulma.min.css',
+                ]
+            },
+            icons: {
+                expand: true,
+                cwd: './node_modules',
+                dest: './img/icons/',
+                flatten: true,
+                filter: 'isFile',
+                timestamp: true,
+                src: [
+                    './feather-icons/dist/feather-sprite.svg',
+                ]
+            },
+            leaflet_images: {
+                expand: true,
+                cwd: './node_modules',
+                dest: './css/images/',
+                flatten: true,
+                filter: 'isFile',
+                timestamp: true,
+                src: [
+                    './leaflet/dist/images/*.png',
+                ]
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/js/*.js'],
+                dest: 'js/searx.js'
+            }
+        },
+        uglify: {
+            options: {
+                sourceMap: true,
+                banner: '/*! oscar/searx.js | <%= grunt.template.today("dd-mm-yyyy") %> | <%= process.env.GIT_URL %>  */\n'
+            },
+            dist: {
+                files: {
+                    'js/searx.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        sass: {
+            development: {
+                options: {
+                    loadPath: ["src/sass", "node_modules/bulma/bulma.sass"]
+                },
+                files: {
+                    "css/penguin-light.min.css": "src/sass/light/penguin.sass",
+                    "css/penguin-dark.min.css": "src/sass/dark/penguin.sass",
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['src/js/**.js'],
+                tasks: ['concat', 'uglify']
+            },
+            penguin_styles: {
+                files: ['src/sass/**/*.sass'],
+                tasks: ['sass']
+            },
         }
-      }
-    },
-    sass: {
-      development: {
-        options: {
-          loadPath: ["src/sass"]
-        },
-        files: {
-          "css/penguin-light.css": "src/sass/light/penguin.sass",
-          "css/penguin-dark.css": "src/sass/dark/penguin.sass",
-        }
-      },
-      production: {
-        options: {
-          loadPath: ["src/sass"],
-        },
-        files: {
-          "css/penguin-light.min.css": "src/sass/light/penguin.sass",
-          "css/penguin-dark.min.css": "src/sass/dark/penguin.sass",
-        }
-      },
-    },
-    watch: {
-        scripts: {
-            files: ['src/js/**.js'],
-            tasks: ['concat', 'uglify']
-        },
-        penguin_styles: {
-            files: ['src/sass/light/**/*.sass', 'src/sass/dark/**/*.sass'],
-            tasks: ['sass:development', 'sass:production']
-        },
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
-  grunt.registerTask('default', ['copy', 'concat', 'uglify', 'sass']);
+    grunt.registerTask('default', ['copy', 'concat', 'uglify', 'sass']);
 
-  grunt.registerTask('styles', ['sass']);
+    grunt.registerTask('styles', ['sass']);
 
 };
